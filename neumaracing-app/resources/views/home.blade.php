74c28229a0aabf30ebad61f1825a875e3faddfb8
Francys999 <francis11444@gmail.com>
2024-10-30 15:12:06 -0500

    Puliendo el Nav

--- neumaracing-app/resources/views/home.blade.php
+++ neumaracing-app/resources/views/home.blade.php
@@ -16,14 +16,14 @@
             <div class="flex items-center space-x-2">
                 <a href="/" class="flex items-center space-x-2">
                     <img src="{{asset("img/email.svg")}}" alt="Email" class="h-6 w-6">
-                    <span>jcnllantas@gmail.com</span>
+                    <span class="font-bold">jcnllantas@gmail.com</span>
                 </a>
             </div>
             <!-- WhatsApp -->
             <div class="flex items-center space-x-0">
                 <a href="/" class="flex items-center space-x-0">
                     <img src="{{asset("img/whatsapp.png")}}" alt="Whatsapp" class="h-8 w-8">
-                    <span>+51 948 464 266</span>
+                    <span class="font-bold">+51 948 464 266</span>
                 </a>
             </div>
         </div>
@@ -33,21 +33,28 @@
         <!-- Sección Izquierda: Logo -->
         <div class="flex items-center space-x-4">
             <a href="/">
-                <img src="{{ asset('img/LogoNeumaRacing.png') }}" alt="Neuma Racing" class="h-11">
+                <img src="{{ asset('img/LogoNav.png') }}" alt="Neuma Racing" class="h-12">
             </a>
         </div> 
         <!-- Sección Central: Barra de Búsqueda -->
         <div class="flex justify-center flex-1">
             <div class="relative w-96">
-                <!-- Imagen de Lupa -->
-                <img src="{{ asset('img/lupa.png') }}" alt="Buscar" class="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5"/>
+                <!-- Imagen de Lupa con un Label -->
+                <label for="search-input" class="absolute left-3 top-1/2 transform -translate-y-1/2 cursor-text">
+                    <img src="{{ asset('img/lupa.png') }}" alt="Buscar" class="h-5 w-5" />
+                </label>
                 <!-- Campo de Entrada -->
                 <input 
+                    id="search-input"
                     type="text" 
                     placeholder="Buscar" 
-                    class="w-full pl-12 pr-4 py-2 rounded-full bg-custom-gray text-white placeholder-gray-400 focus:outline none hover:bg-white hover:bg-opacity-20 transition-colors duration-200"/>
+                    class="w-full pl-12 pr-4 py-2 rounded-full bg-custom-gray text-white 
+                        placeholder-gray-400 focus:outline-none 
+                        hover:bg-white hover:bg-opacity-20 transition-colors duration-200"
+                />
             </div>
         </div>
+
         <!-- Sección Derecha: Íconos de Usuario y Carrito -->
         <div class="flex items-center space-x-6">
             <!-- Ingresar -->
